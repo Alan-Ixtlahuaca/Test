0061b37a4ad8c4a412cc210080c58cb68633d0b9
Alan-Ixtlahuaca-ISC6 <Metalbuckets@outlook.com>
2026-04-28 12:12:06 -0600

    refactor: redesign knowledge base to dynamic symptom evaluation using list-based disease definitions

--- mental_sencillo.pl
+++ mental_sencillo.pl
@@ -1,33 +1,16 @@
-% --- BASE DE HECHOS ---
-% Estos son los datos que "llegarían" de la página HTML.
-% Por ejemplo, si en el HTML Juan marca que tiene tristeza e insomnio:
-padece(juan, tristeza).
-padece(juan, insomnio).
-padece(ana, nerviosismo).
-padece(carlos, miedo).
+:- encoding(utf8).
+:- dynamic si/1.
+:- dynamic no/1.
 
-% Definimos qué síntoma pertenece a qué trastorno (igual que es_sintoma(fiebre, gripa))
-es_sintoma(tristeza, depresion).
-es_sintoma(perdida_interes, depresion).
-es_sintoma(nerviosismo, ansiedad).
-es_sintoma(preocupacion, ansiedad).
-es_sintoma(miedo, panico).
-es_sintoma(insomnio, trastorno_sueno).
+% --- BASE DE CONOCIMIENTO: ENFERMEDADES Y SINTOMAS ---
+enfermedad(depresion, [tristeza, perdida_interes, sin_esperanza]).
+enfermedad(ansiedad, [nerviosismo, preocupacion, tension_muscular]).
+enfermedad(panico, [miedo_intenso, falta_respiracion, taquicardia]).
+enfermedad(trastorno_sueno, [insomnio, despertar_frecuente, cansancio_diurno]).
 
-% Definimos qué tratamiento alivia qué trastorno (igual que suprime(paracetamol, fiebre))
-alivia(terapia_cognitiva, depresion).
-alivia(meditacion, ansiedad).
-alivia(ejercicios_respiracion, panico).
-alivia(melatonina, trastorno_sueno).
-
-% --- REGLAS --- (Misma estructura que usó tu profesor)
-
-% 1. Regla para saber el diagnóstico: Una persona tiene un trastorno si padece un síntoma de ese trastorno
-tiene_trastorno(Persona, Trastorno) :- 
-    padece(Persona, Sintoma), 
-    es_sintoma(Sintoma, Trastorno).
-
-% 2. Regla para saber qué recetar: Debe tomar un tratamiento si tiene un trastorno y ese tratamiento lo alivia
-debe_tomar_terapia(Persona, Tratamiento) :- 
-    tiene_trastorno(Persona, Trastorno), 
-    alivia(Tratamiento, Trastorno).
+% --- REGLAS ---
+% Diagnostica un trastorno si un síntoma afirmativo ("si") está en la lista de síntomas de esa enfermedad
+tiene_trastorno(Trastorno) :-
+    enfermedad(Trastorno, ListaSintomas),
+    si(Sintoma),
+    member(Sintoma, ListaSintomas).
