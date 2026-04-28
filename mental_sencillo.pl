a0706ea86b7fdbeef2593a435df5c175f4bc8e87
Alan-Ixtlahuaca-ISC6 <Metalbuckets@outlook.com>
2026-04-28 12:13:55 -0600

    feat: add diagnostic data for five additional mental health disorders to the knowledge base

--- mental_sencillo.pl
+++ mental_sencillo.pl
@@ -7,6 +7,11 @@ enfermedad(depresion, [tristeza, perdida_interes, sin_esperanza]).
 enfermedad(ansiedad, [nerviosismo, preocupacion, tension_muscular]).
 enfermedad(panico, [miedo_intenso, falta_respiracion, taquicardia]).
 enfermedad(trastorno_sueno, [insomnio, despertar_frecuente, cansancio_diurno]).
+enfermedad(esquizofrenia, [alucinaciones, delirios, pensamiento_desorganizado]).
+enfermedad(trastorno_bipolar, [cambios_humor_extremos, mania_euforia, impulsividad]).
+enfermedad(trastorno_obsesivo_compulsivo, [pensamientos_intrusivos, compulsiones_repetitivas, necesidad_orden]).
+enfermedad(estres_postraumatico, [flashbacks, pesadillas, hipervigilancia]).
+enfermedad(trastorno_alimentacion, [distorsion_imagen_corporal, restriccion_alimentaria, atracones]).
 
 % --- REGLAS ---
 % Diagnostica un trastorno si un síntoma afirmativo ("si") está en la lista de síntomas de esa enfermedad
